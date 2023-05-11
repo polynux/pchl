b0e879be15efa4f3127917598d5dccc27ec78f55
Guillaume Dorce <polynux.dev@gmail.com>
2023-05-11 22:25:22 +0200

    remove eslint plugin

--- src/pages/test-pb.tsx
+++ src/pages/test-pb.tsx
@@ -7,10 +7,10 @@ export default function Test() {
   return (
     <Layout>
       {json.data?.map((page) => (
-        <div key={page.id}>
-          <p>{page.id}</p>
-          <h1>{page.title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: page.field }} />
+        <div key={page?.id}>
+          <p>{page?.id}</p>
+          <h1>{page?.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: page?.field }} />
         </div>
       ))}
     </Layout>
